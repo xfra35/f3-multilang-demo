5e940b6c3ec2e898970bf9b441745f5de58bbbb6
xfra35 <xfra35@gmail.com>
2014-11-25 09:29:52 +0100

    pas de mise en cache sur une erreur

--- usr/app/error.php
+++ usr/app/error.php
@@ -4,9 +4,15 @@ namespace App;
 
 class Error extends _Base {
 
+    /**
+     * Custom Error handler
+     * @param \Base $f3
+     */
     function handler($f3) {
         while (ob_get_level())
             ob_end_clean();
+        if (!headers_sent())
+            $f3->expire(0);
         $f3->set('main','app/ui/pages/error.html');
         echo \Template::instance()->render('app/ui/index.html');
         die();
